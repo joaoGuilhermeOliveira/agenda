f6ff3af568944c7bb489f390fd6fdd3a1c3cb5df
joaoGuilhermeOliveira <joaogoliveira14@gmail.com>
2024-10-17 11:52:57 -0300

    criada função de atualizar contato

--- config/process.php
+++ config/process.php
@@ -11,8 +11,8 @@ $data = $_POST;
 if (!empty($data)) {
 
     // Criar contato
-    if($data['type'] === "create") {
-        
+    if ($data['type'] === "create") {
+
         $name = $data['name'];
         $phone = $data['phone'];
         $observations = $data['observations'];
@@ -26,23 +26,47 @@ if (!empty($data)) {
         $stmt->bindParam(":observations", $observations);
 
         try {
-    
+
             $stmt->execute();
             $_SESSION['msg'] = "Contato criado com sucesso!";
-        
+
         } catch (PDOException $e) {
             //erro na conexão
             $error = $e->getMessage();
             echo "Erro: $error";
         }
 
+    } else if($data["type"] === "edit") {
 
-
+        $name = $data["name"];
+        $phone = $data["phone"];
+        $observations = $data["observations"];
+        $id = $data["id"];
+    
+        $query = "UPDATE contact 
+                  SET name = :name, phone = :phone, observations = :observations
+                  WHERE id = :id";
+    
+        $stmt = $conn->prepare($query);
+    
+        $stmt->bindParam(":name", $name);
+        $stmt->bindParam(":phone", $phone);
+        $stmt->bindParam(":observations", $observations);
+        $stmt->bindParam(":id", $id);
+        
+        try {
+          $stmt->execute();
+          $_SESSION["msg"] = "Contato atualizado com sucesso!";
+        } catch(PDOException $e) {
+          // verificando erro
+          $error = $e->getMessage();
+          echo "Erro: $error";
+        }
     }
 
     // REDIRECT HOME
-    header("location:" .$BASE_URL . "../index.php");
-    
+    header("location:" . $BASE_URL . "../index.php");
+
 // SELEÇÃO DE DADOS
 } else {
     $id;
