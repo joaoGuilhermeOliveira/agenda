5967b57802d1ecca3a3ae6f0a01764f9f28ac5a4
joaoGuilhermeOliveira <joaogoliveira14@gmail.com>
2024-10-17 12:07:32 -0300

    adicionada função para deletar contato

--- config/process.php
+++ config/process.php
@@ -62,6 +62,26 @@ if (!empty($data)) {
           $error = $e->getMessage();
           echo "Erro: $error";
         }
+    } else if ($data['type'] === "delete") {
+
+        $id = $data['id'];
+
+        $query = "DELETE FROM contact WHERE id = :id";
+
+        $stmt = $conn->prepare($query);
+
+        $stmt->bindParam(":id", $id);
+
+
+        try {
+            $stmt->execute();
+            $_SESSION["msg"] = "Contato removido com sucesso!";
+          } catch(PDOException $e) {
+            // verificando erro
+            $error = $e->getMessage();
+            echo "Erro: $error";
+          }
+
     }
 
     // REDIRECT HOME
--- index.php
+++ index.php
@@ -25,7 +25,11 @@ include_once("templates/header.php");
                         <td class="actions">
                             <a href="<?= $BASE_URL ?>show.php?id=<?= $contactx['id'] ?>"><i class="fas fa-eye check-icon"></i></a>
                             <a href="<?= $BASE_URL ?>edit.php?id=<?= $contactx['id'] ?>"><i class="far fa-edit edit-icon"></i></a>
-                            <button type="submit" class="delete-btn"><i class="fas fa-times delete-icon"></i></button>
+                            <form class="delete-form" action="<?= $BASE_URL ?>/config/process.php" method="POST">
+                                <input type="hidden" name="type" value="delete">
+                                <input type="hidden" name="id" value="<?= $contactx['id'] ?>">
+                                <button type="submit" class="delete-btn"><i class="fas fa-times delete-icon"></i></button>
+                            </form>
                         </td>
                     </tr>
                 <?php endforeach; ?>
